538933b2d75b3461e8df6284d26d525132a46d94
ZYX <chnzyx2016@gmail.com>
2024-12-18 22:42:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import sys
 import keyboard
 import time, random
 from pynput.keyboard import Controller
-
-def press_key_pynput(key):
-    while win32gui.GetForegroundWindow() != game_nd:
-        time.sleep(1)
-    keyboard = Controller()
-    keyboard.press(key)
-    time.sleep(random.random()*0.2+0.2)
-    keyboard.release(key)
     
 def set_forground(game_nd):
     try:
@@ -45,6 +37,28 @@ def init_window():
     set_forground(game_nd)
     return game_nd
 
+def press_key_pynput(key):
+    global stop, game_nd
+    cnt = 0
+    while win32gui.GetForegroundWindow() != game_nd:
+        time.sleep(1)
+        if stop:
+            return
+        cnt += 1
+        if cnt > 600:
+            print("尝试将游戏窗口置于前台...")
+            game_nd = init_window()
+            if game_nd is None:
+                print("未找到游戏窗口，停止运行")
+                stop = 1
+                return
+            time.sleep(1)
+            break
+    keyboard = Controller()
+    keyboard.press(key)
+    time.sleep(random.random()*0.2+0.2)
+    keyboard.release(key)
+
 stop = False
 def on_key_press(event):
     if event.name == "f8":
@@ -55,9 +69,10 @@ def on_key_press(event):
 keyboard.on_press(on_key_press)
 game_nd = init_window()
 if game_nd is not None:
+    print('游戏窗口已找到，开始运行')
     time.sleep(1)
     press_key_pynput('f')
     while not stop:
         time.sleep(random.random()*4+2)
         press_key_pynput('f')
-    time.sleep(3)
+    time.sleep(1)
